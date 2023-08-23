67dda6767ea09228a632d34a950d3ff06d73a6f7
Loza <97474199+Lolah101@users.noreply.github.com>
2023-08-23 09:41:11 +0100

    Set up search function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <Dictionary />
+        <footer className="App-footer">
+          <small>Coded by SheCodes</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-
